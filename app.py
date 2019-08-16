17d7ad71e7ecb4d28694ae0390b3eaaaafc4011b
Kathy Nguyen <kathyn262@gmail.com>
2019-08-16 15:38:12 -0700

    obtained guess post value

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from boggle import Boggle
-from flask import Flask, render_template, session
-
+from flask import Flask, render_template, session, request
+import json
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "oh-so-secret"
@@ -14,6 +14,13 @@ def display_board():
     """Display the game board on home page"""
 
     return render_template("home.html", board=board)
+
+
+@app.route("/guess", methods=["POST"])
+def return_guess():
+    # d = request.json["formValue"]
+    print(request.json["guess"])
+    return "hi"
 
 
 @app.route('/board-route')
--- static/app.js
+++ static/app.js
@@ -1,1 +1,9 @@
-console.log("PLEASE WORK")
+async function getFormValue(e) {
+    e.preventDefault();
+    let formValue = $('.input-guess').val()
+    let sendVal = await axios.post("/guess", {guess: formValue})
+    // remember that 2nd argument in axios wants an object
+    // server also wants an object to parse into a string
+}
+
+$('#input-form').on('submit', getFormValue)
